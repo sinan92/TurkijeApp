5f568086addfcdc8684e23c9df5d9d722a82d255
Sinan Samet <sinan.samet@facebook.com>
2018-10-02 10:58:58 -0700

    Initial Commit

---
+++ Turkije App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Turkije App
+//
+//  Created by Sinan Samet on 02/10/2018.
+//  Copyright © 2018 Sinan Samet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
